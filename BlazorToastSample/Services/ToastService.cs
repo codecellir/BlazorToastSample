7590ca77314165f7529906488b97f9706b39b5dc
sohrabgheisari <sohrabgheisari@gmail.com>
2024-02-14 20:24:30 +0330

    Add project files.

---
+++ BlazorToastSample/Services/ToastService.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorToastSample.Services
+{
+    public class ToastService
+    {
+        public event Action<string, ToastLevel>? OnShow;
+        public event Action? OnClose;
+
+        public void ShowToast(string message, ToastLevel level)
+        {
+            OnShow?.Invoke(message, level);
+        }
+
+        public void CloseToast() => OnClose?.Invoke();
+    }
+
+    public enum ToastLevel : byte
+    {
+        Info,
+        Success,
+        Warning,
+        Error
+    };
+    public enum ToastPosition : byte
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+        Center
+    };
+}
